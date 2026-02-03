1de1bda65b7b10849ac195ca5bb95ad4d9218c1c
zuckerburg <ahujaaditya97@gmail.com>
2026-02-03 15:58:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Aditya",
+    valentineName: "Swati",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
